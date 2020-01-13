39d1fa0a0a764484838356cedb75f9bb4ffc50f2
werike Santos <werike.ms@gmail.com>
2020-01-13 19:15:59 -0300

    Finalizado aula 20

---
+++ 3-Estrutura_Sequencial/aula_20/src/funcoesMatematicas/parte01.java
@@ -0,0 +1,48 @@
+package funcoesMatematicas;
+
+import java.util.Scanner;
+
+public class parte01 {
+
+	public static void main(String []args) {
+		
+		//ALGUMAS FUNÇÕES MATEMÁTICAS EM JAVA
+		
+		/* EXEMPLO
+			A = Math.sqrt(x);
+			VARIÁVEL (A) RECEBE A RAIZ QUADRADA DE X
+			
+			A = Math.pow(x, y);
+			VARIÁVEL (A) RECEBE O RESULTADO DE X ELEVADO A Y
+			
+			A = Math.abs(x);
+			VARIÁVEL (A) RECEBE O VALOR ABSOLUTO DE X
+		*/
+		
+		
+		double x = 3.0;
+		double y = 4.0;
+		double z = -5.0;
+		double A, B, C;
+		
+		A = Math.sqrt(x);
+		B = Math.sqrt(y);
+		C = Math.sqrt(25.0);
+		System.out.println("Raiz quadrada de " + x + " = " + A);
+		System.out.println("Raiz quadrada de " + y + " = " + B);
+		System.out.println("Raiz quadrada de 25 = " + C);
+		
+		A = Math.pow(x, y);
+		B = Math.pow(x, 2.0);
+		C = Math.pow(5.0, 2.0);
+		System.out.println(x + " elevado a " + y + " = " + A);
+		System.out.println(x + " elevado ao quadrado = " + B);
+		System.out.println("5 elevado ao quadrado = " + C);
+		
+		A = Math.abs(y);
+		B = Math.abs(z);
+		System.out.println("Valor absoluto de " + y + " = " + A);
+		System.out.println("Valor absoluto de " + z + " = " + B);
+		
+	}
+}
